3733cc88af47c76d82b3e6ae42de24338f07428b
Juloja <j.lo.jaramillo@gmail.com>
2024-12-04 22:15:05 -0500

    subiendo a vercel

--- src/Routes/Favs.jsx
+++ src/Routes/Favs.jsx
@@ -2,7 +2,6 @@ import React, { useContext } from "react";
 import { ContextGlobal } from "../Components/utils/global.context";
 import Card from "../Components/Card";
 
-//Este componente deberá ser estilado como "dark" o "light" dependiendo del theme del Context
 
 const Favs = () => {
   const { state } = useContext(ContextGlobal);
--- src/Routes/Home.jsx
+++ src/Routes/Home.jsx
@@ -2,7 +2,6 @@ import { useEffect, useState } from 'react'
 import React from 'react'
 import Card from '../Components/Card'
 
-//Este componente debera ser estilado como "dark" o "light" dependiendo del theme del Context
 
 const Home = () => {
   const [dentists, setDentists] = useState([]);
